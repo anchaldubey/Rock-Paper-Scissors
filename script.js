97705558e17f1d6ab30d9af497845172c2e2cc10
anchaldubey <118725568+anchaldubey@users.noreply.github.com>
2023-04-07 13:00:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var you;
+var yourscore = 0;
+var opponent;
+var opponentscore = 0;
+
+var choices = ["rock", "paper", "scissors"];
+
+window.onload = function() {
+    for (let i = 0; i < 3; i++) {
+        // <img id="rock" src="rock.png">
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice);
+    }
+}
+
+function selectChoice() {
+    you = this.id;
+    document.getElementById("your-choice").src = you + ".png";
+}
